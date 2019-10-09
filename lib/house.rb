f73e91e954504b327281e3bad1926569816dc70c
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-10-09 09:59:19 -0600

    Rooms from category method and test

--- lib/house.rb
+++ lib/house.rb
@@ -7,27 +7,34 @@
 # pry(main)> house = House.new("$400000", "123 sugar lane")
 # #=> #<House:0x00007fccd30375f8...>
 #
-# pry(main)> house.price
-# #=> 400000
-#
-# pry(main)> house.address
-# #=> "123 sugar lane"
-#
-# pry(main)> house.rooms
-# #=> []
-#
 # pry(main)> room_1 = Room.new(:bedroom, 10, 13)
 # #=> #<Room:0x00007fccd29b5720...>
 #
 # pry(main)> room_2 = Room.new(:bedroom, 11, 15)
 # #=> #<Room:0x00007fccd2985f48...>
 #
+# pry(main)> room_3 = Room.new(:living_room, 25, 15)
+# #=> #<Room:0x00007fccd383c2d0...>
+#
+# pry(main)> room_4 = Room.new(:basement, 30, 41)
+# #=> #<Room:0x00007fccd297dc30...>
+#
 # pry(main)> house.add_room(room_1)
 #
 # pry(main)> house.add_room(room_2)
 #
-# pry(main)> house.rooms
+# pry(main)> house.add_room(room_3)
+#
+# pry(main)> house.add_room(room_4)
+#
+# pry(main)> house.rooms_from_category(:bedroom)
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
+#
+# pry(main)> house.rooms_from_category(:basement)
+# #=> [#<Room:0x00007fccd297dc30...>]
+#
+# pry(main)> house.area
+# #=> 1900
 
 class House
   attr_reader :price, :address, :rooms
@@ -40,4 +47,17 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def rooms_from_category(room_cat)
+    rooms_by_cat = @rooms.find_all {|room| room.category == room_cat}
+    rooms_by_cat
+  end
+    # rooms_by_cat = []
+    # @rooms.each do |room|
+    #   if room.category = room_cat
+    #     rooms_by_cat << room
+    #   end
+    # end
+    # rooms_by_cat
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -10,6 +10,8 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, 13)
     @room_2 = Room.new(:bedroom, 11, 15)
+    @room_3 = Room.new(:living_room, 25, 15)
+    @room_4 = Room.new(:basement, 30, 41)
   end
 
   def test_it_exists
@@ -27,7 +29,22 @@ class HouseTest < Minitest::Test
     @house.add_room(@room_2)
     assert_equal true, @house.rooms.include?(@room_1)
     assert_equal true, @house.rooms.include?(@room_2)
-    assert_equal [@room_1, @room_2], @house.rooms
+    # assert_equal [@room_1, @room_2], @house.rooms
+  end
+
+  def test_rooms_from_category
+    assert_equal [], @house.rooms_from_category(:bedroom)
+
+    @house.add_room(@room_1)
+    assert_equal [], @house.rooms_from_category(:basement)
+    assert_equal [@room_1], @house.rooms_from_category(:bedroom)
+
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    assert_equal true, @house.rooms_from_category(:bedroom).include?(@room_2)
+    assert_equal [@room_3], @house.rooms_from_category(:living_room)
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
   end
 end
 
